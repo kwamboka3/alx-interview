a3e9cd0b59efee460671df906a55dd757eb791d9
kwamboka3 <pkwamboka@flag42.com>
2023-11-10 10:43:28 +0300

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,54 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const URL = 'https://swapi-api.hbtn.io/api/films/';
-request.get(URL + process.argv[2], async (error, response, body) => {
-  if (response.statusCode === 200 && error === null) {
-    const linkChars = JSON.parse(body).characters;
-    const linkCharsList = chr => {
-      const promise = new Promise((resolve, reject) => {
-        request.get(chr, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(body);
-          }
-        });
-      });
-      return promise;
-    };
-    for (let i = 0; i < linkChars.length; i++) {
-      const result = await linkCharsList(linkChars[i]);
-      console.log(JSON.parse(result).name);
-    }
+// Get Movie ID from Command Line
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Usage: ./0-starwars_characters.js <Movie_ID>');
+  process.exit(1);
+}
+
+// Make API Request
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error || response.statusCode !== 200) {
+    console.error(`Error: Unable to fetch data. Status Code: ${response ? response.statusCode : 'Unknown'}`);
+    process.exit(1);
   }
+
+  // Extract Characters
+  const characters = JSON.parse(body).characters;
+
+  // Display Characters Sequentially
+  displayCharactersSequentially(characters);
 });
+
+// Display Characters Sequentially
+function displayCharactersSequentially (characters) {
+  const index = 0;
+
+  function fetchCharacter (index) {
+    if (index >= characters.length) {
+      // All characters fetched, exit the process
+      process.exit(0);
+    }
+
+    const characterUrl = characters[index];
+    request(characterUrl, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        console.error(`Error fetching character: ${error ? error.message : 'Unknown'}`);
+      } else {
+        console.log(JSON.parse(body).name);
+      }
+
+      // Fetch the next character
+      fetchCharacter(index + 1);
+    });
+  }
+
+  // Start fetching characters
+  fetchCharacter(index);
+}
